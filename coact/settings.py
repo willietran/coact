083b22d314fc16f2353872618d22e44b46cc2f72
Willie Tran <tran.william26@gmail.com>
2014-07-31 17:26:53 -0700

    Deploying to Heroku

--- coact/settings.py
+++ coact/settings.py
@@ -41,9 +41,7 @@ INSTALLED_APPS = (
     'south',
     'mptt',
     'easy_thumbnails',
-    'calendarium',
     'django_messages',
-    'crispy_forms',
     'registration',
     'stripe',
     'braintree',
@@ -137,22 +135,6 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = 'tran.william26@gmail.com'
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
