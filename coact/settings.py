1e17cb08a9d1a953d5798c07f1c8e2113d01f5e4
Willie Tran <tran.william26@gmail.com>
2014-08-01 14:48:38 -0700

    Push to Heroku

--- coact/settings.py
+++ coact/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'filer'
+    'filer',
     'south',
     'mptt',
     'easy_thumbnails',
