340e929971c4e4626d0a7f3ec7399ccaa8fbf898
Willie Tran <tran.william26@gmail.com>
2014-08-19 13:48:48 -0700

    Switched back to local server settings

--- local_settings.py
+++ local_settings.py
@@ -1,20 +1,20 @@
 __author__ = 'WillieTran'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'coact',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd4kr09egpvr8q4',
-        'USER': 'rchyzmbmdfpfow',
-        'PORT': '5432',
-        'PASSWORD': 'ADcPMBPSNcnJEKayRu8TreaHrS',
-        'HOST': 'ec2-54-197-241-79.compute-1.amazonaws.com'
+        'NAME': 'coact',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd4kr09egpvr8q4',
+#         'USER': 'rchyzmbmdfpfow',
+#         'PORT': '5432',
+#         'PASSWORD': 'ADcPMBPSNcnJEKayRu8TreaHrS',
+#         'HOST': 'ec2-54-197-241-79.compute-1.amazonaws.com'
+#     }
+# }
---
+++ marketplace/static/js/view_profile.js
@@ -0,0 +1,8 @@
+/**
+ * Created by WillieTran on 8/18/14.
+ */
+//$.fn.editable.defaults.mode = 'inline';
+//
+//$(document).ready(function() {
+//    $('.user-name').editable();
+//});
